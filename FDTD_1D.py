84bd6dd919bbbcf038cb108824fe8f25d2649224
Astral <robinzhang0112@gmail.com>
2023-12-04 17:11:27 -0800

    re-implement algorithms for correction and clarity

--- FDTD_1D.py
+++ FDTD_1D.py
@@ -2,14 +2,26 @@
 
 import numpy as np
 
-def field_evolve_vacuum(E_0 : np.ndarray, B_0 : np.ndarray, dx, dt, step):
+def advance_E_field(E : np.ndarray, B : np.ndarray, dx, dt):
+    n = E.shape[0]
+    for i in range(n):
+        E[i] += dt / dx * (B[i] - B[i-1])
+
+def advance_B_field(E : np.ndarray, B : np.ndarray, dx, dt):
+    n = B.shape[0]
+    for i in range(n-1):
+        B[i] += dt / dx * (E[i+1] - E[i])
+    B[n-1] += dt / dx * (E[0] - E[n-1])
+
+
+def field_evolve_vacuum(E : np.ndarray, B : np.ndarray, dx, dt):
     """Compute evolution of electromagnetic field in vacuum in 1D using the
     FDTD method. All quantities are assumed to be dimensionless.
 
     Inputs:
-    - E_0 : Initial electric field at integer coordinates, t=-dt/2. Assume
+    - E : Initial electric field at integer coordinates, t=-dt/2. Assume
       periodic boundary conditions
-    - B_0 : Initial magnetic field at half-integer coordinates, t=0. 
+    - B : Initial magnetic field at half-integer coordinates, t=0. 
     - dx : Standard grid size of the fields.
     - dt : Time step of field evolution.
     - step : Total number of time steps to be simulated. The system will generate
@@ -22,19 +34,6 @@ def field_evolve_vacuum(E_0 : np.ndarray, B_0 : np.ndarray, dx, dt, step):
     - B_history: Numpy array containing history of magnetic field evolution.
     """
     
-    grid_size = E_0.shape[0]
-    E_history = np.zeros((step + 1, grid_size))
-    B_history = np.zeros((step + 1, grid_size))
-    E_history[0] = E_0.copy()
-    B_history[0] = B_0.copy()
-    for i in range(step):
-        # update Electric field
-        for j in range(1, grid_size):
-            E_history[i+1][j] = E_history[i][j] - dt / dx * (B_history[i][j] - B_history[i][j-1])
-        E_history[i+1][0] = E_history[i][0] - dt / dx * (B_history[i][0] - B_history[i][grid_size - 1])
-        # update magnetic field
-        for k in range(grid_size - 1):
-            B_history[i+1][k] = B_history[i][k] - dt / dx * (E_history[i][k+1] - E_history[i][k])
-        B_history[i+1][grid_size - 1] = B_history[i][grid_size - 1] - dt / dx * (E_history[i][0] - E_history[i][grid_size - 1])
-
-    return E_history, B_history
+    advance_E_field(E, B, dx, dt)
+    advance_B_field(E, B, dx, dt)
+    return E, B
