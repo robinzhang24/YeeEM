3721cf055a49ef1d892e8948dafa21d7723e70af
Astral <robinzhang0112@gmail.com>
2023-12-08 14:56:42 -0800

    1D medium implementation

--- FDTD_1D.py
+++ FDTD_1D.py
@@ -13,8 +13,8 @@ def advance_E_field_vacuum(E: np.ndarray, B: np.ndarray, dx, dt):
 def advance_B_field_vacuum(E: np.ndarray, B: np.ndarray, dx, dt):#same in vacuum and medium
     n = B.shape[0]
     for i in range(n - 1):
-        B[i] += -dt / dx * (E[i + 1] - E[i])
-    B[n - 1] += -dt / dx * (E[0] - E[n - 1])
+        B[i] += dt / dx * (E[i + 1] - E[i])
+    B[n - 1] += dt / dx * (E[0] - E[n - 1])
 
 
 def field_evolve_vacuum(E: np.ndarray, B: np.ndarray, dx, dt):
@@ -41,16 +41,19 @@ def field_evolve_vacuum(E: np.ndarray, B: np.ndarray, dx, dt):
     advance_B_field_vacuum(E, B, dx, dt)
     return E, B
 
-def advance_JE_medium(E : np.ndarray, B : np.ndarray, J: np.ndarray, dx, dt, omega_p, nu):
+def advance_JE_medium(E : np.ndarray, B : np.ndarray, J: np.ndarray, Omega_p: np.ndarray,
+                       nu: np.ndarray, dx, dt):
     n = E.shape[0]
-    J[0] += (omega_p ** 2 / 4 / np.pi * E[0] - nu * J[0]) / dt
-    E[0] += dt / dx * (B[0] - B[n - 1]) - 4 * np.pi * J[0] / dt
+    J_old = J.copy()
+    J[0] += (Omega_p[0] ** 2 / 4 / np.pi * E[0] - nu[0] * J[0]) / dt
+    E[0] += dt / dx * (B[0] - B[n - 1]) - 4 * np.pi * J_old[0] / dt
     for i in range(1, n):
-        J[i] += (omega_p ** 2/4/np.pi * E[i] - nu * J[i]) / dt
-        E[i] += dt / dx * (B[i] - B[i - 1]) - 4 * np.pi * J[i] / dt
+        J[i] += (Omega_p[i] ** 2/4/np.pi * E[i] - nu[i] * J[i]) / dt
+        E[i] += dt / dx * (B[i] - B[i - 1]) - 4 * np.pi * J_old[i] / dt
 
-def field_evolve_medium(E: np.ndarray, B: np.ndarray, J: np.array, dx, dt, omega_p, nu):
+def field_evolve_medium(E: np.ndarray, B: np.ndarray, J: np.ndarray, Omega_p: np.ndarray,
+                        nu: np.ndarray, dx, dt):
 
-    advance_JE_medium()(E, B, dx, dt, omega_p, nu)
+    advance_JE_medium(E, B, J, Omega_p, nu, dx, dt)
     advance_B_field_vacuum(E, B, dx, dt)
     return E, B, J
